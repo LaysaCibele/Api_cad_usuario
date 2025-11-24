19d3cbb53ee8ae2a54c7ffb10ecc8443dde8a586
Laysa <laysacibele23@gmail.com>
2025-11-24 11:22:42 -0300

    feat: adicionando login (front - html, css, js)

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,17 @@
+//Nova etapa: Aqui que vai acontecer a troca entre cadastro e login
+
+function mostrarLogin(){
+    document.getElementById('form-cadastro').style.display = "none";
+    document.getElementById('form-login').style.display = "block";
+}
+
+
+function mostrarCadastro(){
+    document.getElementById('form-login').style.display = "none";
+    document.getElementById('form-cadastro').style.display = "block";
+}
+
+//Meu JS de cadastro (antes do login)
 const form = document.getElementById('cadForm');
 
 form.addEventListener('submit', async(evento) => {
@@ -26,4 +40,32 @@ form.addEventListener('submit', async(evento) => {
     else{
         alert('ERRO. Falha no cadastro.')
     }
-})
+});
+
+//Nova etapa do JS (Agora temos o login)
+
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', async(evento) => {
+    evento.preventDefault();
+
+    const dadosLogin = {
+        email: document.getElementById('loginEmail').value,
+        senha: document.getElementById('loginSenha').value
+    };
+
+        const resposta = await fetch('/api/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(dadosLogin) //Aqui eu converto o objeto JS em JSON
+    });
+
+    const data = await resposta.json();
+
+    if (resposta.ok){
+        alert("Login realizado!")
+    }
+    else{
+        alert("data.erro");
+    }
+});
